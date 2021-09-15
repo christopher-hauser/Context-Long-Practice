8bdcb4b3032f43817384ebfa2c04b6ac626b09a5
Kyle Huang <kylehuang@Kyles-MacBook-Pro.local>
2021-09-15 12:32:09 -0700

    first

--- problems/01-car-drive.js
+++ problems/01-car-drive.js
@@ -1,4 +1,23 @@
 // Your code here
+/*
+1. Create a `Car` class.
+2. A newly instantiated instance should have its `speed` property initialized to
+   `0`.
+3. Add an instance method called `drive(newSpeed)` that takes in a `newSpeed`
+   and sets it to the instance's `speed` property. It should also return the
+   present `speed` of the instance.
+
+*/
+class Car{
+	constructor(){
+		this.speed = 0;
+	}
+
+	drive(newSpeed){
+		this.speed = newSpeed;
+		return this.speed;
+	}
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
--- test/01-car-drive-spec.js
+++ test/01-car-drive-spec.js
@@ -5,6 +5,7 @@ const Car = require("../problems/01-car-drive.js");
 describe("Car's drive() method", function () {
   it("should return the speed of the car after calling drive()", function () {
     let car = new Car();
+    
     let test1 = car.drive(10);
     let result1 = car.speed;
 
