633dab45064f794c27dee78e74b79b276cab35a0
Kyle Huang <kylehuang@Kyles-MacBook-Pro.local>
2021-09-15 13:11:15 -0700

    problem 6 done

--- problems/06-fancy-calculator.js
+++ problems/06-fancy-calculator.js
@@ -1,4 +1,32 @@
-// Your code here
+const Calculator = require("./02-calculator")
+
+class FancyCalculator extends Calculator{
+	constructor(){
+		super();
+	}
+	setTotal(newTotal){
+		this.total = newTotal;
+		return this.total;
+	}
+	modulo(num){
+		this.total = this.total % num;
+		return this.total;
+	}
+	squared(){
+		this.total *= this.total;
+		return this.total;
+	}
+}
+/*
+1. Import the `Calculator` class.
+2. Create a `FancyCalculator` class with the `Calculator` class as its parent
+   class.
+3. Add the following instance methods which should all return the `total`
+   property of the instance:
+   a. `setTotal(newTotal)` - sets the `total` to the `newTotal`
+   b. `modulo(num)` - sets the `total` to the remainder of dividing by `num`
+   c. `squared()` - multiplies the `total` by its self
+*/
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
