3853bb48826ec03b4a71406645315c262bdec842
Kyle Huang <kylehuang@Kyles-MacBook-Pro.local>
2021-09-15 12:58:03 -0700

    problem 3 solved

--- problems/02-calculator.js
+++ problems/02-calculator.js
@@ -1,6 +1,15 @@
-// Your code here
-
-
+/*
+1. Create a `Calculator` class.
+2. A newly instantiated instance should have its `total` property initialized to
+   `0`.
+3. Add the following instance methods which should all return the `total`
+   property of the instance:
+   a. `add(num)` - add the `num` arg to the `total`
+   b. `subtract(num)` - subtract the `num` arg from the `total`
+   c. `divide(num)` - divide the `total` by the `num` arg
+   d. `multiply(num)` - multiply the `total` by the `num` arg
+*/
+console.log(this);
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
--- problems/03-make-dog.js
+++ problems/03-make-dog.js
@@ -1,6 +1,31 @@
 // Your code here
 
+/*
+1. Create a `Dog` class.
+2. The `constructor` function should take in a `name` argument and set the
+   `name` property of a newly instantiated instance to the `name` argument.
+3. Add a static method called `makeJet()` that will return a new instance of the
+   `Dog` class with a `name` property of "Jet".
+4. Add two instance methods:
+   a. `changeName(newName)` - set the `name` to the `newName`
+   b. `speak(word)` - returns `${name} says ${word}`
+*/
+class Dog{
+	constructor(name){
+		this.name = name;
+	}
 
+	static makeJet(){
+		const jet = new Dog("Jet");
+		return jet;
+	}
+	changeName(newName){
+		this.name = newName;
+	}
+	speak(word){
+		return `${this.name} says ${word}`
+	}
+}
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
